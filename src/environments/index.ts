0a7893c0c6d5cccc1929f2bf746e2259169e3991
marcegdv <mgdv79@gmail.com>
2024-04-27 15:11:52 -0300

    env host

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -1,7 +1,7 @@
 export type Environment = keyof typeof environment;
 
 const environment = {
-    HOST: process.env.HOST || '',
+    HOST: process.env.HOST || 'http://localhost',
     PORT: Number(process.env.PORT) || 5050,
     API_DATENAGER_URL: process.env.API_DATENAGER_URL || "https://date.nager.at",
     API_DATENAGER_PATH_PUBLICHOLYDAYS: process.env.API_DATENAGER_URL || "/api/v3/publicholidays",
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const PORT: number = env.PORT;
 try {
     api.listen(PORT, () => console.log(msg));
     const msg: string =
-        `\nServer listening on ${HOST}:${PORT}\n`;
+        `\nServer listening on ${HOST}:${PORT}/\n`;
 } catch (error) {
     console.log('Error: Can not listen on port', PORT);
 };
