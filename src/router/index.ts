ec37f537643d6bd8c270bfb64a088e628ef5c1a7
marcegdv <mgdv79@gmail.com>
2024-04-27 17:22:35 -0300

    path params. for holydays endpoint

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import express, { Router } from 'express';
 import { homeControllerExample } from '../controllers/home';
 import { healthControllerExample } from '../controllers/health';
-import { dateNagerController } from '../controllers/dateNager';
+import { datenagerController } from '../controllers/datenager';
 
 const router: Router = express.Router();
 
 router.get('/', homeControllerExample);
 router.get('/health', healthControllerExample);
 
-router.get("/holydays", dateNagerController);
+router.get("/holydays/:year/:countryCode", datenagerController);
 
 export default router;
