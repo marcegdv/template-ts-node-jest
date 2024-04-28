916d2f12709516c2f56c0ce6ba9f3c325f624537
marcegdv <mgdv79@gmail.com>
2024-04-28 02:34:11 -0300

    merge master

--- src/controllers/health.ts
+++ src/controllers/health.ts
@@ -2,7 +2,13 @@ import { NextFunction, Request, Response } from "express";
 
 export const healthResponse = { status: 'ok' };
 
-export async function healthControllerExample(req: Request, res: Response, next: NextFunction) {
+export async function healthControllerExample(
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> {
+
     res.json(healthResponse);
     next();
+
 };
--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,8 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 
-export const homeHTML: string = '<html><head><title>Node App</title></head><body style=\"color: white; background-color: black;\"><h1>API online.</h1></body></html>'
+export const homeHTML: string = "<html><head><title>Node App</title></head><body style=\"color: white; background-color: black;\"><h1>API online.</h1></body></html>";
 
-export async function homeControllerExample(req: Request, res: Response, next: NextFunction) {
+export async function homeControllerExample(
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> {
+
     res.status(200).send(homeHTML);
     next();
+
 };
